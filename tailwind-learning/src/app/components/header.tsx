91472565cb45066455f08321157a8d813f767b21
AndranikS-Solicy <andranik.sargsyan@solicy.net>
2024-08-09 16:46:17 +0400

    Created separate component for MenuIcon

--- tailwind-learning/src/app/components/header.tsx
+++ tailwind-learning/src/app/components/header.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import ImageComponent from "./image";
 import Button from "./button";
+import MenuIcon from "./menuIcon";
 
 const Header: React.FC = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
@@ -42,20 +43,7 @@ const Header: React.FC = () => {
             onClick={toggleMenu}
             className="text-warmGray focus:outline-none"
           >
-            <svg
-              className="w-6 h-6"
-              fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16m-7 6h7"
-              ></path>
-            </svg>
+            <MenuIcon />
           </Button>
         </div>
       </div>
